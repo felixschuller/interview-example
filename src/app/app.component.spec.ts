f3d6bc19299350bf2878c5b490aa3d203c86fb87
Felix <felix.schuller@dedalus.eu>
2022-02-28 15:06:47 +0100

    node 16 lts, simple unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,40 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should have title', () => {
+    const titleElem = fixture.debugElement.query(By.css('.title'));
+
+    expect(titleElem.nativeElement.innerHTML).toBe('Interview example');
+  });
+
+  it('should emit name after 10ms', () => {
+    let name;
+    
+    app.getNameAfterTenMS().subscribe(emitted => name = emitted);
+
+    expect(name).toBe('Hera');
+  });
+
 
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,9 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(`Example application initialized with name: ${this.name}`);
+  }
+
+  getNameAfterTenMS(): Observable<string> {
+    return timer(10).pipe(map(() => this.name));
   }
 }
